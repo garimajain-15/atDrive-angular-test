3a9b770d6c9f5c8a2a67fb3fc00c198bef1a9d2f
Garima Jain <garima@hiddentalent.tech>
2024-02-27 17:22:24 +0530

    At-drive-test: added routings in the application & designed top header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserListComponent} from "./feature-components/user-list/user-list.component";
+import {HeaderComponent} from "./feature-components/header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'users', component: UserListComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './feature-components/user-list/user-list.component';
+import { HeaderComponent } from './feature-components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
